42f45dbe8f4362d5c9ef99119f0b80ea0a4b6790
yunrong <18855606463@163.com>
2019-11-21 18:07:34 +0800

    commit by yunrong

--- promise/index.js
+++ promise/index.js
@@ -53,10 +53,13 @@
    const _self = this //保存上下文
 
    function resolve(value){
-     console.log('status',_self.status)
+     console.log('resolve: _self',_self)
+     console.log('resolve p3',p3)
+     console.log('resolve p1',p1)
+     console.log('resolve p2',p2)
      if(_self.status === 'pending'){
        setTimeout(() => {
-        console.log('times')
+         console.log('setTimeout _self',_self)
         _self.status = 'fulfilled' //将状态置为完成
         _self.result = value //将传入的value作为解释
         console.log(_self.resolveCallbackCol)
@@ -80,6 +83,7 @@
    }
 
    try {
+    console.log('new a myPromise')
     executor(resolve,reject)
    } catch (error) {
      reject(error)
@@ -101,16 +105,16 @@
     }
 
     if(retValue instanceof myPromise){ //如果是myPromise的实例
-      // if(retValue.status === 'pending'){ //且状态为pending状态,先注册then方法，然后等待执行
-      //   retValue.then(result => { 
-      //     dealMyPromise(retPromise,result,resolve,reject)
-      //   },error => {
-      //     reject(error)
-      //   })
-      // }else{//如果不是pending，直接注册执行
-      //   retValue.then(resolve,reject)
-      // }
-      retValue.then(resolve,reject) //注册then
+      if(retValue.status === 'pending'){ //且状态为pending状态,先注册then方法，然后等待执行
+        retValue.then(result => { 
+          dealMyPromise(retPromise,result,resolve,reject)
+        },error => {
+          reject(error)
+        })
+      }else{//如果不是pending，直接注册执行
+        retValue.then(resolve,reject)
+      }
+      // retValue.then(resolve,reject) //注册then
     }else{//如果是普通的值
       resolve(retValue) //直接resolve
     }
@@ -131,6 +135,7 @@
 
    //判断then方法传入的参数是否是个function，如果不是则直接忽略 
    console.log(_self.status)
+   console.log(_self)
 
    onResolve = judgeFunc(onResolve) === flag ? onResolve : function(value){return value} //return value的原因是可以让值一直传递
    onReject = judgeFunc(onReject) === flag ? onReject : function(value){return value}
@@ -142,11 +147,16 @@
       return retPromise = new myPromise((resolve,reject) => {
         console.log('beforeResolve','  resolveCallbackCol push')
         _self.resolveCallbackCol.push(function(value){
-          try {
-            console.log('afterResolve')
-            let result = onResolve(_self.result) //先拿到返回值
-            console.log('pending result',result)
-            console.log(_self.result)
+        try {
+            console.log('nowStatus',_self.status)
+            console.log('before onResolve _self',_self)
+            console.log('before onResolve p3',p3)
+            console.log('onResolve',onResolve)
+            let result = onResolve(_self.result) //先拿到返回值console.log('')
+            console.log('onResolve next p3',p3)
+            console.log('onResolve next p2',p2)
+            console.log('onResolve next _self',_self)
+            console.log('onResolve next result',result)
             dealMyPromise(retPromise,result,resolve,reject)
 
           } catch (error) {
@@ -212,20 +222,26 @@
   */
 
  let p1 = new myPromise((resolve,reject) => {
+   console.log('first exec')
    setTimeout(() => {
      console.log('p1')
      resolve('111')
    }, 4);
  })
+ p1.name = 'p1'
 
- let p3
+ let p3 = {}
+ p3.name = 'p3'
 
  let p2 = p1.then((data)=> {
-   p3 = new myPromise((resolve,reject)=> {
+   p3.promise = new myPromise((resolve,reject)=> {
+     console.log('ddd',2222)
      resolve('222')
    })
-   return p3
+   return p3.promise
  })
+
+ p2.name = 'p2'
 
  p2.then((data)=> {
    console.log('ccc',data)
@@ -235,46 +251,7 @@
   * 运行结果如下：
   * 
   * 
-  * pending //因为p1 executor里面是一段setTimeOut，所以代码会被推入任务队列。所以执行 p1 的 then 方法 在此打印了pending
-    beforeResolve   resolveCallbackCol push //因为状态时pending，所以在此打印 标识着p1的 resolveCallbackCol 和 rejectCallbackCol 都被推入了一个对应的函数
-    pending //然后调用第二个then方法 继续判断 p1的状态，因为此时resolve还未执行，所以状态pending （其实不用setTimeOut模拟异步，resolve里的状态也还是未更新的，因为resolve是异步的）
-    beforeResolve   resolveCallbackCol push //然后向p2的队列里面推入对应的函数
-    p1 //执行setTimeOut 打印p1
-    status pending //执行resolve方法 打印此时的状态
-    times //打印times
-    [ [Function] ] //打印队列中的函数
-    afterResolve //执行函数
-    status pending //再调用一遍resolve方法 因为是异步的，所以延迟执行
-    result myPromise { //打印result 第一个then里面 返回的是一个myPromise
-      status: 'pending',
-      resolveCallbackCol: [],
-      rejectCallbackCol: [],
-      result: null }
-    111 //打印上一次resolve的值
-    dealMyPromise exec! //调用dealMyPromise方法
-    pending  //因为是一个myPromise的实例，继续调用then方法，此时的状态是pending
-    beforeResolve   resolveCallbackCol push //推入对应的数组
-    times //执行p3的resolve方法，实际上调用then方法里面被推入的方法
-    [ [Function] ] //打印p3的函数
-    afterResolve // 执行函数
-    status pending //p3又调用了一次resolve，继续被推入队列，等待执行
-    result undefined //因为并没有返回结果 所以未undefined
-    222 //输出上一次resolve的值
-    dealMyPromise exec! //再次执行dealMyPromise
-    status pending //因为是一个普通值，所以调用resolve，再次被推入任务队列，等待执行 error
-    times //注意 如果返回了一个myPromise并且resolve会自动调用then方法 （因为已经注册，所以函数不是空的），
-          //然后这个promise会返回给一个匿名的promise，继续resolve，所以打印times
-    [ [Function] ] //打印p2的函数队列
-    afterResolve //执行函数
-    ccc 222 //函数输出结果
-    result undefined //因为无返回值，所以打印undefined 
-    222 //然后执行之前因为调用onResolve的方法
-    dealMyPromise exec!
-    status pending
-    times
-    []
-    times
-    []
+  * 
   */
 
 
