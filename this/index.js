599e743371db911ec2e69392d5f59b56395b6080
yunrongliu <616149185@qq.com>
2019-12-21 15:31:22 +0800

    lc

---
+++ this/index.js
@@ -0,0 +1,125 @@
+/**
+ * 此篇描述this
+ */
+
+ // this的绑定方式
+
+ // 1. 默认绑定 default        
+{
+  function defaultFn () {
+    console.log(this)
+  }
+ 
+  defaultFn() // Object global
+}
+ 
+
+
+ // 2. 隐式绑定 implicit
+
+{
+  let obj = {
+    name: 'yr',
+    implicitFn: function () {
+      console.log(this.name)
+    }
+  }
+
+  let obj2 = {
+    target: obj,
+    name: 'lc'
+  }
+
+  obj.implicitFn() //yr
+  obj2.target.implicitFn() //yr
+
+  /** 隐式绑定只在最后一层调用位置起作用 */
+
+  // 隐式丢失
+
+  let other = obj.implicitFn
+
+  other() // undefined 因为global没有name
+
+  global.name = 'global'
+
+  other() // global
+
+  /**
+   * 方法声明后 地址是在堆上的 
+   * 一个对象的属性为是个function，只不过是保存了函数的引用，也就是说，这个函数并不属于任何对象或者某个属性
+   * 将对象的属性赋值给一个变量后，传递的只是引用。
+   * 函数本身的this绑定是在函数运行时决定的，只有箭头函数是在定义时决定的。这涉及到js的执行环境。
+   */
+
+  let another = {
+    name: 'cc',
+    implicitFn: obj.implicitFn
+  }
+
+  another.implicitFn() // cc
+}
+
+// 3. 显示绑定 display
+{
+  let obj1 = {
+    name: 'obj1',
+    displayFn: function (args) {
+      console.log(this.name,args)
+    }
+  }
+
+  let obj2 = {
+    name: 'obj2'
+  }
+
+  obj1.displayFn.call(obj2,1) // obj2 1
+
+  obj1.displayFn.apply(obj2,[1]) //obj2 1
+
+  let bindObj = obj1.displayFn.bind(obj2,1) 
+  bindObj() //obj2 1
+  let obj3 = {
+    name: 'obj3'
+  }
+
+  bindObj.call(obj3,1) // obj2 1
+}
+
+// 4. new 绑定
+{
+  function person (name) {
+    this.name = name
+  }
+
+  let p1 = new person('yr')
+
+  console.log(p1.name) // yr
+} 
+
+/**
+ * 优先级
+ * 从上面的代码来看 
+ *  1. 默认绑定的优先级最低
+ *  2. 其次是隐式绑定
+ *  3. 显示绑定与new绑定的优先级别还未确定
+ */
+
+// 验证
+
+{
+  function test (num) {
+    this.num = num 
+  }
+
+  let obj = {}
+
+  let testObj = test.bind(obj)
+  testObj(2) // 2
+
+  console.log(obj.num)
+
+  let other = new testObj(3)
+  console.log(other.num) // 3
+  console.log(obj.num) // 2
+}
--- 设计模式/发布订阅模式/index.js
+++ 设计模式/发布订阅模式/index.js
@@ -1,59 +1,158 @@
-{
-  let pubsub = (function(){
-    /**
-     * @private center {object} 发布订阅调度中心
-     * @private  evName  {string} 模块名 
-     */
-    let center = null,
-        evName = 'default'
+let pubsub = {
+  /**
+   * 订阅
+   * @param name {string} 订阅事件的名称 
+   * @param fn {function} 订阅事件的处理函数
+   */
+  _subscribe(name = 'default',fn){
+    console.log('this',this.name)
+    let obj = this.catchHash.get(name)
+    console.log('obj',obj)
+    this.index += 1
+    let id = this.index
+    if(!obj){
+      obj = {}
+      obj.name = name
+      obj.fnStack = []
+      this.catchHash.set(name,obj)
+      // this.catch[name] = obj
+    }
+    console.log('fnStack',obj.fnStack)
+    obj.fnStack.push({
+      fn,
+      id
+    })
+  },
+  /**
+   * 发布
+   * @param name 要发布的事件名称
+   * @param data 对应的数据
+   */
+  _publish(){
+    let [name = 'default',...data] = [...arguments]
+    let obj = this.catchHash.get(name)
+    // if(!Array.isArray(this.catch[name].cbStack) || !this.catch[name].cbStack.length) return;
+    if(!Array.isArray(obj.fnStack) || !obj.fnStack.length) return;
+    return this.execStackFn(obj.fnStack,data)
+  },
+  /**
+   * 订阅一次
+   */
 
-    center = {
-      cache: {}, //{object}当前模块名对应的用于储存fn的对象
-      /**
-       * 发布消息的方法
-       * @param name {string} 要订阅的消息的事件名称 
-       * @param data {arguments} 触发方法需要的参数
-       */
-      publish(name,data){
-        /**
-         * @private fnStack {array} 回调函数栈
-         */
-        let fnStack = this.cache[name]
-        if(Object.prototype.toString.call(fnStack) === '[object Array]' && fnStack.length > 0){
-          return  this.execFnStack(fnStack,data)
-        }
-
-      },
-      /**
-       * 订阅消息的方法
-       * @param name {string} 发布消息的事件名称 
-       * @param fn {function} 订阅者订阅时对应的函数
-       */
+  /**
+   *  取消订阅
+   * @param name {string} 取消某一名称的订阅事件
+   * @param key {number} 取消通知某一个订阅者的通知
+   */
+  _unsubscribe(name,key){
+    let obj = this.catchHash.get(name)
+    if(!obj){
+      return;
+    }
+    if (!key) {
+      this.catchHash.delete(name)
+      return;
+      // Reflect.deleteProperty(this.catch,key)
+    }
+    obj.fnStack.forEach((val,inx) => {
+      if(val.id === key){
+        obj.fnStack.splice(inx,1)
+        return;
+      }
+    })
+  },
+  /**
+   * 通过别名的方式
+   * @param moduleName {'string'}
+   */
+  create(moduleName = 'default'){
+    if(this.nameSpaceCatch && this.nameSpaceCatch[moduleName]) return this.nameSpaceCatch[moduleName]
+    let psInstance = Object.create(this)
+    this.name = moduleName
+    this.catchHash = new Map(), //根据不同的name 储存对应的函数队列的map
+    // catch: {}, //传统对象
+    this.index = -1, //每个订阅者的唯一id
+    this.offlineStackHash = new Map(),//离线事件存储
+    this.nameSpaceCatch = {}//命名空间
+    let ret = {
       subscribe(name,fn){
-
-        if(!this.cache[name]){
-          this.cache[name] = [] 
+        psInstance._subscribe(name,fn)
+        let obj = psInstance.offlineStackHash.get(name)
+        if(!obj){
+          obj = {}
+          obj.name = name
+          psInstance.offlineStackHash.set(name,obj)
         }
-        this.cache[name].push(fn) 
+        if(obj.offlineStack === null || obj.offlineStack === undefined) return;
+        psInstance.execStackFn(obj.offlineStack)
+        obj.offlineStack = null
       },
-      /**
-       * 取消订阅的方法
-       * @param target 要取消订阅的方法名 或者 唯一标识
-       */
-      unsubscribe(name){
-        Reflect.deleteProperty(this.catch,name)
+      publish(){
+        let [name = 'default',...data] = [...arguments]
+        let obj = psInstance.offlineStackHash.get(name)
+        if(!obj){
+          obj = {}
+          obj.name = name
+          obj.offlineStack = []
+          psInstance.offlineStackHash.set(name,obj)
+        }
+        let fn = function(){
+          return psInstance._publish(name,data)
+        }
+        if(obj.offlineStack){
+          return obj.offlineStack.push(fn)
+        }
+
+        return fn()
       },
-      /**
-       * 执行函数栈
-       * @param fnStack {array} 函数栈 
-       * @param data {arguments} 参数
-       * @return ret {Array} 函数返回结果  ?? 如果执行多个函数 那么只能获取到单一结果?
-       */
-      execFnStack(fnStack,data){
-        return fnStack.reduce((ret, current) => {
-          ret.push(current.call(this,data))
-        }, [])
+      unsubscribe(name,key){
+        psInstance._unsubscribe(name,key)
       }
     }
-  })()
+    return this.nameSpaceCatch[moduleName] ? this.nameSpaceCatch[moduleName] : this.nameSpaceCatch[moduleName] = ret
+  },
+  subscribe(name,fn){
+    let obj = this.create()
+    obj.subscribe(name,fn)
+  },
+  publish(){
+    let obj = this.create()
+    obj.publish.apply(this,arguments)
+  },
+  unsubscribe(name,key){
+    let obj = this.create()
+    obj.unsubscribe(name,key)
+  },
+  /**
+   * 循环执行函数
+   * @param fnStack 函数栈对象 
+   * {
+   *  fn 方法
+   *  id 唯一标识
+   * }  
+   */
+  execStackFn(fnStack,data){
+    console.log('execStackFn',fnStack,data)
+    return fnStack.reduce((ret,obj) => {
+      let res = data ? obj.fn.call(this,obj.id,...data) : obj.fn()  
+      ret.push(res)
+      return ret
+    },[])
+  }
 }
+
+
+
+pubsub.subscribe('test',function(id,data){
+  console.log('default',id,data)
+})
+
+pubsub.publish('test',1)
+
+pubsub.create('myu').subscribe('test',function(id,data){
+  console.log('myu',id,data)
+})
+
+pubsub.create('myu').publish('test',2)
+
+
